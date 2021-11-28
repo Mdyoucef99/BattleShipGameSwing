b29ea6c3ed71a3babf724ee2f786670d8f0bff70
Youcef <mekki.d.youcef@outlook.fr>
2021-11-28 10:39:11 -0500

    Ajout classe manquante et fix projet

--- BatailleNavalePartie3/pack/Participant.java
+++ BatailleNavalePartie3/pack/Participant.java
@@ -1,5 +1,43 @@
 package pack;
 
-public class Participant {
+public class Participant  implements InterfaceParticipant
+{
+
+	@Override
+	public void genereNouvelleFlotte() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Flotte getFlotte() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean flotteARecuTirQuiATouche(Coord tir) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean jeuEstTermine() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getDernierNavireCoule() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean dernierTirACoule() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 
 }
